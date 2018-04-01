aa50081109ee5a86e05666b9da162063b18f2e8a
Pedro Henrique Mognon <phm1993@gmail.com>
2018-04-01 16:36:07 -0300

    Finished landing page example

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import './src/js/bootstrap';
 
 $(document).ready(() => {
 	$('.header').height($(window).height());
+
+	$(".navbar a").click(function(){
+		$("body,html").animate({
+			scrollTop:$("#" + $(this).data('value')).offset().top
+		},1000)
+
+	})
 });
